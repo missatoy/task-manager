b2519ea675c99236d88a86125607529f42f9283c
rubensy <86684948+rubensy@users.noreply.github.com>
2022-09-12 17:22:30 +0200

    Implemented routes, actions and created index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,40 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
+  def show
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to bookmarks_path(anchor: "bookmark_#{@bookmark.id}"), notice: "Updated successfully"
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(@task).permit(:name, :description)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
